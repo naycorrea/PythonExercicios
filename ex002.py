f292f82e16a0ed32424ca3548244238d5d94e617
nayaracorrea <43212442+nayaracorrea@users.noreply.github.com>
2020-11-23 09:55:38 -0300

    exercicio

---
+++ ex002.py
@@ -0,0 +1,8 @@
+nome = input('Digite seu nome: ')
+
+#Primeira opção de print
+#print('é um prazer te conhecer, ', nome)
+
+#Segunda opção de print
+print('É um prazer te conhecer, {}!'.format(nome))
+
